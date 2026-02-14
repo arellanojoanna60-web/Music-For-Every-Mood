b2d49d1f85dfc233a2ba78e7ae2b5c8acf4d2bea
IndulgeCoffeeph <arellanojoanna60@gmail.com>
2026-02-14 21:26:01 +0800

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function startListening() {
-    alert("Welcome to WaveStream! Start exploring music now.");
+function playSong(url) {
+    const player = document.getElementById("audioPlayer");
+    player.src = url;
+    player.play();
+
+    const playlist = document.getElementById("playlistItems");
+    const li = document.createElement("li");
+    li.textContent = "Now Playing: " + url.split("/").pop();
+    playlist.appendChild(li);
+}
+
+function openModal() {
+    document.getElementById("authModal").style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("authModal").style.display = "none";
 }
